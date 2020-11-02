23b341dbd541a84d2078e7e760117223a9761865
julymole <64532834+julymole@users.noreply.github.com>
2020-11-02 19:56:32 +0300

    push

---
+++ dist/js/script.js
@@ -0,0 +1,39 @@
+////alert('Hello, gulp!');
+
+$(document).ready(function () {
+$('figure:eq(2)').addClass('disabled');
+  $('figure').on('mouseenter', function (event) {
+    event.preventDefault();
+    if ($(this).hasClass('selected')) {
+      $(this).addClass('selectedHover');
+    } else if ($(this).hasClass('disabled')) {
+      console.log('disabled');
+    } else {
+      $(this).addClass('defaultHover');
+    }
+  });
+  $('figure').on('mouseleave', function (event) {
+    event.preventDefault();
+    if ($(this).hasClass('selectedHover')) {
+      $(this).removeClass('selectedHover');
+    } else if ($(this).hasClass('disabled')) {
+      console.log('disabled');
+    } else {
+      $(this).removeClass('defaultHover');
+    }
+  });
+
+  $('figure').on('click', function (event) {
+    event.preventDefault();
+    if ($(this).hasClass('selected')) {
+      $(this).removeClass('selected');
+      $(this).removeClass('selectedHover');
+    } else if ($(this).hasClass('disabled')) {
+      console.log('disabled');
+    } else {
+      $(this).removeClass('defaultHover');
+      $(this).addClass('selected');
+
+    }
+  });
+});
